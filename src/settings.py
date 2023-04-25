ebba772ec5d0680c3c3c547ea50ecc79d7f0d4f4
Alexandr Starkov <a.starkovjr@gmail.com>
2023-04-25 12:15:14 +0600

    create images folder, minor code optimization

--- src/settings.py
+++ src/settings.py
@@ -3,7 +3,7 @@ class Settings():
     
     def __init__(self):
         self.screen_width = 1200
-        self.screnn_height = 800
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         
-    
+  
