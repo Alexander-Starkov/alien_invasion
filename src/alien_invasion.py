3b4b4b35f0a742860a422b5ff33972cc34533113
Alexandr Starkov <a.starkovjr@gmail.com>
2023-04-28 12:34:49 +0600

    Small changes

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from ship import Ship
 
 from settings import Settings
 
@@ -13,6 +14,8 @@ class AlienInvasion():
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -24,6 +27,7 @@ class AlienInvasion():
             
             # При каждом проходе цикла перерисовывается экран.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
