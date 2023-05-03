aba88da1a5f491ebd3a00400fb08f851ead60cd3
Alexandr Starkov <a.starkovjr@gmail.com>
2023-05-03 14:31:48 +0600

    add Alien class and alien image

---
+++ src/aline.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """Класс, с одним пришельцем"""
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        
+        #* Загрузка изображения пришельца и назначения стартовой позиции через атрибут rect
+        self.image = pygame.image.load('images/frog.bmp')
+        self.rect = self.image.get_rect()
+        
+        #* Появление пришельцев в верхнем левом углу
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        
+        #* Сохранение точной горизонтальной позиции пришельца
+        self.x = float(self.rect.x)
+        
+        
+        
